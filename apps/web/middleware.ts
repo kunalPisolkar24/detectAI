0e450a3e562fa65b1acaac40970857ae78b8dd75
Kunal Pisolkar <kpisolkar24@gmail.com>
2025-03-16 11:50:45 +0000

    added ratelimitter

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,16 +1,32 @@
-// middleware.js
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
+import rateLimit from '@/config/rateLimit';
 
-export async function middleware(req: any) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-
+export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
-  if (pathname.startsWith('/chat') && !token) {
-    const url = req.nextUrl.clone();
-    url.pathname = '/login';
-    return NextResponse.redirect(url);
+  if (!pathname.startsWith('/public/')) {
+    // @ts-ignore: req.ip might not be available in all environments
+    const identifier = req.ip ?? '127.0.0.1';
+    const isAllowed = await rateLimit.limit(identifier);
+
+    if (!isAllowed.success) {
+      return new NextResponse(null, {
+        status: 429,
+        statusText: 'Too Many Requests',
+        headers: { 'Content-Type': 'text/plain' },
+      });
+    }
+  }
+
+  if (pathname.startsWith('/chat')) {
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    if (!token) {
+      const url = req.nextUrl.clone();
+      url.pathname = '/login';
+      return NextResponse.redirect(url);
+    }
   }
 
   return NextResponse.next();
