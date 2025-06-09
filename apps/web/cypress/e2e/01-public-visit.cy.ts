86a7fb0b2184c056216cb0ebc61706f348a7449e
Kunal Pisolkar <kpisolkar24@gmail.com>
2025-06-09 04:52:14 +0000

    completed public visit test

--- apps/web/cypress/e2e/01-public-visit.cy.ts
+++ apps/web/cypress/e2e/01-public-visit.cy.ts
@@ -73,15 +73,16 @@ describe('Public Page Navigation and Verification', () => {
       cy.contains('h1', /Get Started/i).should('be.visible');
     });
   });
-  // context('Navigation to Docs Page ("/docs")', () => {
-  //   it('should navigate via Navbar and display specific content', () => {
-  //     cy.visit('/docs');
-  //     cy.url().should('include', '/docs');
-  //     cy.contains('h2', /DetectAI API Documentation/i).should('be.visible').wait(2000);
-  //     cy.contains(
-  //       'p',
-  //       /API documentation for the DetectAI Next\.js application, covering authentication, user management, model interactions, and billing./i,
-  //     ).should('be.visible');
-  //   });
-  // });
+
+  context('Navigation to Docs Page ("/docs")', () => {
+    it('should navigate via Navbar and display specific content', () => {
+      cy.visit('/docs');
+      cy.url().should('include', '/docs');
+      cy.contains('h2', /DetectAI API Documentation/i).should('be.visible').wait(9000);
+      cy.contains(
+        'p',
+        /API documentation for the DetectAI Next\.js application, covering authentication, user management, model interactions, and billing./i,
+      ).should('be.visible');
+    });
+  });
 });
