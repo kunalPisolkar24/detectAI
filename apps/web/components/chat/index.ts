144e9d1f1ef528c99a966198cd73fbe712a69c04
kunalPisolkar24 <kpisolkar24@gmail.com>
2025-12-30 19:39:42 +0530

    remove complete code

--- apps/web/components/chat/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./chat-nav";
-export * from "./chat-interface"
-export * from "./chat-page"
-export * from "./chat-item"
-export * from "./app-sidebar"
--- apps/web/config/rateLimit.ts
+++
@@ -1,9 +0,0 @@
-import { Ratelimit } from "@upstash/ratelimit";
-import redis from "./redisDB"
-
-const rateLimiter = new Ratelimit({
-  redis: redis,
-  limiter: Ratelimit.fixedWindow(50, "60 s"), 
-});
-
-export default rateLimiter;
