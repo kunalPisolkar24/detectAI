2c44a355ec5c85a7628f2a8e837fa732ed9d9d49
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-01-01 09:30:53 +0530

    add experimental optimizations

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,12 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "avatar.vercel.sh",
       },
     ],
+  },
+  experimental: {
+    optimizePackageImports: ["lucide-react", "framer-motion"],
   },
 };
 
