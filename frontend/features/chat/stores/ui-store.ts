7cebdda79e3989a3e47acc146806098644f63b98
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-01-02 19:34:34 +0530

    remove input from store

--- frontend/features/chat/stores/ui-store.ts
+++ frontend/features/chat/stores/ui-store.ts
@@ -3,18 +3,14 @@ import { ModelType } from "../types"
 
 interface ChatUIState {
   selectedModel: ModelType
-  input: string
   currentChatId: string | null
   setModel: (model: ModelType) => void
-  setInput: (value: string) => void
   setCurrentChatId: (id: string | null) => void
 }
 
 export const useChatUIStore = create<ChatUIState>((set) => ({
   selectedModel: "spark",
-  input: "",
   currentChatId: null,
   setModel: (model) => set({ selectedModel: model }),
-  setInput: (value) => set({ input: value }),
   setCurrentChatId: (id) => set({ currentChatId: id })
 }))
