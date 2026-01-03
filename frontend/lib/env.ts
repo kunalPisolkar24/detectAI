7d7ffc1443d402467eccc536632040bde2838652
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-01-04 00:01:15 +0530

    add paddle client in env

--- frontend/lib/env.ts
+++ frontend/lib/env.ts
@@ -16,6 +16,7 @@ export const env = createEnv({
   },
   client: {
     NEXT_PUBLIC_TURNSTILE_SITE_KEY: z.string().min(1),
+    NEXT_PUBLIC_PADDLE_CLIENT_TOKEN: z.string().min(1),
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
@@ -29,6 +30,7 @@ export const env = createEnv({
     TURNSTILE_SECRET_KEY: process.env.TURNSTILE_SECRET_KEY,
     REDIS_URL: process.env.REDIS_URL,
     NEXT_PUBLIC_TURNSTILE_SITE_KEY: process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY,
+    NEXT_PUBLIC_PADDLE_CLIENT_TOKEN: process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN,
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   emptyStringAsUndefined: true,
