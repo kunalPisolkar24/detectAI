7378b3021a69cc1346f9a08b4818450b29b629fb
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-01-03 11:37:28 +0530

    add session provider in layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { ThemeProvider } from "@/providers";
 import { LazyMotionProvider } from "@/components/providers/lazy-motion-provider";
 import { QueryProvider } from "@/components/providers/query-provider";
+import { SessionProvider } from "@/components/providers/session-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { inter } from "@/lib/fonts";
 import "./globals.css";
@@ -19,19 +20,21 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} antialiased min-h-screen flex flex-col`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <QueryProvider>
-            <LazyMotionProvider>
-              {children}
-              <Toaster position="bottom-right" />
-            </LazyMotionProvider>
-          </QueryProvider>
-        </ThemeProvider>
+        <SessionProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <QueryProvider>
+              <LazyMotionProvider>
+                {children}
+                <Toaster position="bottom-right" />
+              </LazyMotionProvider>
+            </QueryProvider>
+          </ThemeProvider>
+        </SessionProvider>
       </body>
     </html>
   );
