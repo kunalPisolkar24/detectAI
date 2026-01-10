e74fe28cca3e201ba2a72da753148a2c008c8590
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-01-10 19:17:14 +0530

    add grpc server

---
+++ services/inference/src/server/grpc_server.py
@@ -0,0 +1,41 @@
+import grpc
+from concurrent import futures
+import signal
+import threading
+from src.generated import ai_service_pb2_grpc
+from src.server.servicers import AIService
+from src.server.interceptors import AuthInterceptor
+from src.server.health import add_health_check
+from src.config import settings
+import structlog
+
+logger = structlog.get_logger()
+
+class GRPCServer:
+    def __init__(self, spark_engine, flare_engine):
+        self.port = settings.GRPC_PORT
+        self.server = grpc.server(
+            futures.ThreadPoolExecutor(max_workers=50),
+            interceptors=[AuthInterceptor()]
+        )
+        
+        servicer = AIService(spark_engine, flare_engine)
+        ai_service_pb2_grpc.add_AIServiceServicer_to_server(servicer, self.server)
+        add_health_check(self.server)
+        
+        self.done_event = threading.Event()
+
+    def start(self):
+        self.server.add_insecure_port(f'[::]:{self.port}')
+        self.server.start()
+        logger.info("server_started", port=self.port)
+        
+        signal.signal(signal.SIGTERM, self._stop_handler)
+        signal.signal(signal.SIGINT, self._stop_handler)
+        
+        self.done_event.wait()
+
+    def _stop_handler(self, signum, frame):
+        logger.info("shutdown_signal_received")
+        self.done_event.set()
+        self.server.stop(grace=10)
