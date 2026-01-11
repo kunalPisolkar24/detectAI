0d7dbf5219730243b64dcc52e1f36fb415a0b102
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-01-11 21:28:20 +0530

    add redis cluster client

---
+++ services/chats/internal/adapters/secondary/repository/redis/client.go
@@ -0,0 +1,26 @@
+package redis
+
+import (
+	"context"
+	"time"
+
+	"github.com/kunalPisolkar24/detectAI/services/chats/internal/config"
+	"github.com/redis/go-redis/v9"
+)
+
+func NewClusterClient(cfg *config.Config) (*redis.ClusterClient, error) {
+	rdb := redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs:        cfg.RedisClusterAddrs,
+		Password:     cfg.RedisPassword,
+		PoolSize:     100,
+		MinIdleConns: 10,
+		ReadTimeout:  3 * time.Second,
+		WriteTimeout: 3 * time.Second,
+	})
+
+	if err := rdb.Ping(context.Background()).Err(); err != nil {
+		return nil, err
+	}
+
+	return rdb, nil
+}
