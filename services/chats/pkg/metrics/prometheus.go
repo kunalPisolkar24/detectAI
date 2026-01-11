07982582beda00a564c295eff49122a92bf679cb
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-01-11 22:52:04 +0530

    Added latency histograms and cache tracking

--- services/chats/pkg/metrics/prometheus.go
+++ services/chats/pkg/metrics/prometheus.go
@@ -14,15 +14,31 @@ var (
 		Name: "chat_messages_ingested_total",
 		Help: "Total number of messages successfully ingested into MongoDB",
 	})
+	
 	StreamLag = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "chat_redis_stream_lag",
 		Help: "Current lag of the Redis stream consumer group",
 	}, []string{"partition"})
+
+	CacheHits = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "chat_cache_hits_total",
+		Help: "Total number of cache hits",
+	})
+
+	CacheMisses = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "chat_cache_misses_total",
+		Help: "Total number of cache misses",
+	})
+
+	RequestLatency = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "grpc_request_duration_seconds",
+		Help:    "Time taken to process gRPC requests",
+		Buckets: prometheus.DefBuckets,
+	}, []string{"method", "status"})
 )
 
 func Init() {
-	prometheus.MustRegister(MessagesIngested)
-	prometheus.MustRegister(StreamLag)
+	prometheus.MustRegister(MessagesIngested, StreamLag, CacheHits, CacheMisses, RequestLatency)
 }
 
 func StartMetricsServer(port string) {
