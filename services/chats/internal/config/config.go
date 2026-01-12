b2158010df41fd30180cd23886fa6996627fc4c1
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-01-12 10:12:51 +0530

    add .env config

--- services/chats/internal/config/config.go
+++ services/chats/internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/kelseyhightower/envconfig"
 )
 
@@ -23,6 +24,8 @@ type Config struct {
 }
 
 func Load() (*Config, error) {
+	_ = godotenv.Load()
+
 	var cfg Config
 	err := envconfig.Process("", &cfg)
 	if err != nil {
