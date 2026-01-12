42739e67533da518ec15efae07372f47f89a02ce
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-01-12 19:21:38 +0530

    update chat repo

--- services/chats/internal/core/ports/chat_repository.go
+++ services/chats/internal/core/ports/chat_repository.go
@@ -9,6 +9,7 @@ import (
 type ChatCacheRepository interface {
 	SaveToCache(ctx context.Context, msg *domain.Message) error
 	GetRecentMessages(ctx context.Context, chatID string) ([]*domain.Message, error)
+	DeleteCache(ctx context.Context, chatID string) error
 }
 
 type ChatStreamRepository interface {
@@ -18,6 +19,10 @@ type ChatStreamRepository interface {
 type ChatPersistenceRepository interface {
 	CreateChat(ctx context.Context, chat *domain.ChatSession) error
 	GetChat(ctx context.Context, chatID string) (*domain.ChatSession, error)
+	GetUserChats(ctx context.Context, userID string, limit int) ([]*domain.ChatSession, error)
+	UpdateChatTitle(ctx context.Context, chatID, title string) error
+	DeleteChat(ctx context.Context, chatID string) error
+	
 	BulkUpsertMessages(ctx context.Context, messages []*domain.Message) error
 	GetHistory(ctx context.Context, chatID string, offset, limit int) ([]*domain.Message, error)
 }
