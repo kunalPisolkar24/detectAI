d035abb0ff89b19db29782910c3e05e293012c87
Kunal Pisolkar <kpisolkar24@gmail.com>
2026-05-16 11:45:04 +0000

    Implement DLQ and error metrics in Prometheus provider

--- services/chats/pkg/metrics/prometheus.go
+++ services/chats/pkg/metrics/prometheus.go
@@ -35,6 +35,21 @@ var (
 		Help:    "Time taken to process gRPC requests",
 		Buckets: prometheus.DefBuckets,
 	}, []string{"method", "status"})
+
+	DLQMessages = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "chat_dlq_messages_total",
+		Help: "Total number of messages moved to the dead letter queue",
+	})
+
+	StreamErrors = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "chat_stream_errors_total",
+		Help: "Total number of Redis stream errors",
+	}, []string{"operation"})
+
+	DatabaseErrors = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "chat_database_errors_total",
+		Help: "Total number of database operation errors",
+	}, []string{"operation"})
 )
 
 type PrometheusMetrics struct{}
@@ -59,8 +74,29 @@ func (p *PrometheusMetrics) SetStreamLag(partition string, lag float64) {
 	StreamLag.WithLabelValues(partition).Set(lag)
 }
 
+func (p *PrometheusMetrics) IncDLQMessages(count float64) {
+	DLQMessages.Add(count)
+}
+
+func (p *PrometheusMetrics) IncStreamErrors(operation string) {
+	StreamErrors.WithLabelValues(operation).Inc()
+}
+
+func (p *PrometheusMetrics) IncDatabaseErrors(operation string) {
+	DatabaseErrors.WithLabelValues(operation).Inc()
+}
+
 func Init() {
-	prometheus.MustRegister(MessagesIngested, StreamLag, CacheHits, CacheMisses, RequestLatency)
+	prometheus.MustRegister(
+		MessagesIngested,
+		StreamLag,
+		CacheHits,
+		CacheMisses,
+		RequestLatency,
+		DLQMessages,
+		StreamErrors,
+		DatabaseErrors,
+	)
 }
 
 func StartMetricsServer(port string) {
